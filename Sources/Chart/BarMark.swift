7fd726378bd0d814de60cfcc9ff1b695faf29a40
Joe Heck <heckj@mac.com>
2022-03-25 16:51:16 -0700

    WIP: MarkBuilder and what a VisualChannel actually is

--- Sources/Chart/BarMark.swift
+++ Sources/Chart/BarMark.swift
@@ -4,12 +4,20 @@
 //
 //  Created by Joseph Heck on 3/25/22.
 //
+import SwiftUI
 
+/// A type that represents a series of bars.
+///
+/// The type infers the number and visual properties of the bars from the data you provide to the visual channels when declaring a bar mark.
 public struct BarMark: Mark {
+    public var x: CGFloat
     
-    let height: Double //? value cast from with quantitative (Double) or Ordinal (Int) type
+    public var y: CGFloat
     
-    let width: Double //? value cast from the mapping to width, which could be from a categorical (String) or ordinal (Int) type.
+    
+    let height: CGFloat //? value cast from with quantitative (Double) or Ordinal (Int) type
+    
+    let width: CGFloat //? value cast from the mapping to width, which could be from a categorical (String) or ordinal (Int) type.
     // The actual width for an individual bar as displayed on the Canvas
     // can be inferred if the mapped property is categorical by dividing
     // the available space among the # of categories being displayed with
