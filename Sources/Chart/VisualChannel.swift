cf95538404379752db4879a05f362cbe2e043ee3
Joe Heck <heckj@mac.com>
2022-03-25 16:30:16 -0700

    stub out VisualChannel
    start to work on sorting out what these types need to be and how to
    arrange a mapping concept from a property on an object back to a
    concrete type that can be used to generate the details for one or more
    marks.

---
+++ Sources/Chart/VisualChannel.swift
@@ -0,0 +1,108 @@
+//
+//  VisualChannel.swift
+//  
+//
+//  Created by Joseph Heck on 3/25/22.
+//
+
+import Foundation
+
+/// A type that can be encoded into the visual property of a mark.
+public protocol TypeOfVisualProperty {
+    var visualPropertyType: VisualPropertyType { get }
+    //var visualPropertyValue: VisualPropertyInstance { get }
+}
+
+extension Double: TypeOfVisualProperty {
+    public var visualPropertyType: VisualPropertyType {
+        .quantitative
+    }
+//    public var visualPropertyValue: VisualPropertyInstance {
+//        return VisualPropertyInstance.quantitative(self)
+//    }
+}
+extension Int: TypeOfVisualProperty {
+    public var visualPropertyType: VisualPropertyType {
+        .ordinal
+    }
+//    public var visualPropertyValue: VisualPropertyInstance {
+//        return VisualPropertyInstance.ordinal(self)
+//    }
+}
+extension String: TypeOfVisualProperty {
+    public var visualPropertyType: VisualPropertyType {
+        .categorical
+    }
+//    public var visualPropertyValue: VisualPropertyInstance {
+//        return VisualPropertyInstance.categorical(self)
+//    }
+}
+extension Date: TypeOfVisualProperty {
+    public var visualPropertyType: VisualPropertyType {
+        .temporal
+    }
+//    public var visualPropertyValue: VisualPropertyInstance {
+//        return VisualPropertyInstance.temporal(self)
+//    }
+}
+
+public enum VisualPropertyType {
+    case quantitative //(Double)
+    case ordinal // (Int)
+    case temporal // (Date)
+    case categorical // (String)
+}
+
+//public struct VisualPropertyInstance<UnderlyingType> {
+//    let type: VisualPropertyType
+//    let value: UnderlyingType
+//    
+//    public init(_ type: VisualPropertyType, _ value: UnderlyingType) {
+//        self.type = type
+//        switch type {
+//        case .quantitative:
+//            self.value = value
+//        case .ordinal:
+//            self.value = value
+//        case .temporal:
+//            self.value = value
+//        case .categorical:
+//            self.value = value
+//        }
+//    }
+//}
+
+
+// struct vs. enum?
+public struct VisualProperty<VisualPropertyInstance> {
+    let name: String
+    let typeAndValue: VisualPropertyInstance
+    
+    // Nope: Static stored properties not supported in generic types,
+    // plus this means we need to know the value as we're defining it,
+    // and we won't at that point.
+    //static var x = VisualProperty(name: "x", typeAndValue: .quantitative(0))
+}
+
+/// A type that provides a the channel mapping to link a property or value to a visual property of a mark.
+public struct VisualChannel<VisualPropertyInstance> {
+    // visual property type of 'Quantitative' accepts value types of 'Double'
+    // visual property type of 'Ordinal' accepts value types of 'Int'
+    // visual property type of 'Categorical' accepts value types of 'String'
+    // visual property type of 'Temporal' accepts value types of 'Date'
+    
+    
+    // something like `VisualChannel(width, \.node)` or `VisualChannel(y, 13)`
+    // What's the type of 'width' and/or 'y' in this case? An enumeration case?
+    // A string literal that's converted to a specific mark property?
+    // Is what we're creating a concrete visual property that evaluates the mapping and can be immediately used, or has the mapping properties that something else can use?
+    
+    // The plan is for whatever this struct is and does to be consumed by
+    // a result builder that creates an instance of a Mark. Does that change
+    // what type a VisualChannel is and what it's responsible for?
+    
+    public init(_ channelName: VisualProperty<VisualPropertyInstance>, _ value: VisualPropertyType) {
+        
+    }
+    
+}
