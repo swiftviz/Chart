7bd892652a6fd89ddcee74f7d330427c9c2b6a97
Joe Heck <heckj@mac.com>
2022-03-25 15:32:08 -0700

    stub in Mark, BarMark, DotMark, and LineMark
    no implemntation, only stubs for BarMark, DotMark, and LineMark,
    all of which conform to the (empty) protocol Mark

---
+++ Sources/Chart/BarMark.swift
@@ -0,0 +1,12 @@
+//
+//  BarMark.swift
+//  
+//
+//  Created by Joseph Heck on 3/25/22.
+//
+
+import Foundation
+
+public struct BarMark: Mark {
+    public init() {}
+}
---
+++ Sources/Chart/Mark.swift
@@ -0,0 +1,11 @@
+//
+//  Mark.swift
+//  
+//
+//  Created by Joseph Heck on 3/25/22.
+//
+
+
+public protocol Mark {
+    
+}
