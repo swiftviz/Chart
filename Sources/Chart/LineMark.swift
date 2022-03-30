ff4b7fcb59ae1b9db53f1d9f427586f405d2cf92
Joe Heck <heckj@mac.com>
2022-03-30 15:37:24 -0700

    simplify VisualChannel
    - remove writeable keypath concept from VisualChannel
    - remove type erasure for now (comment out)
    - reset protocol to just be generic over the data type and the type of
      property being applied

--- Sources/Chart/LineMark.swift
+++ Sources/Chart/LineMark.swift
@@ -12,10 +12,10 @@ import SwiftUI
 /// The type infers the number and visual properties of the bars from the data you provide to the visual channels when declaring a bar mark.
 public struct LineMark: Mark {
     public typealias DataType = Any
-    public var mappings: [AnyVisualChannel<LineMark, Any>]
+//    public var mappings: [AnyVisualChannel<LineMark, Any>]
 
     public typealias MarkType = Self
     public init() {
-        mappings = []
+//        mappings = []
     }
 }
