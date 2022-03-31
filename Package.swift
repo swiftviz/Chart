17ca9a9b1a1fcb06e911a8d4380f0305a49ffce9
Joe Heck <heckj@mac.com>
2022-03-31 11:54:38 -0700

    migrate dependency to use Scale instead of SwiftViz

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .macOS("12"),
         .iOS("15.0"),
         .tvOS("15.0"),
-        .watchOS("8.0"),
+        .watchOS("8.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -19,22 +19,22 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftviz/SwiftViz.git", from: "0.5.0"),
+        .package(url: "https://github.com/swiftviz/scale.git", from: "0.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Chart",
-            dependencies: ["SwiftViz"]
+            dependencies: [.product(name: "SwiftVizScale", package: "Scale")]
         ),
         .testTarget(
             name: "ChartTests",
-            dependencies: ["Chart", "SwiftViz"]
+            dependencies: ["Chart", .product(name: "SwiftVizScale", package: "Scale")]
         ),
         .testTarget(
             name: "DocTests",
-            dependencies: ["Chart"]
+            dependencies: ["Chart", .product(name: "SwiftVizScale", package: "Scale")]
         ),
     ]
 )
