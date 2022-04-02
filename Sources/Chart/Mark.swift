cbd7968eb0242fe5954bd1e82f6c16417abbd60e
Joe Heck <heckj@mac.com>
2022-04-02 14:20:43 -0700

    roughing in lower level shapes that a mark generates

--- Sources/Chart/Mark.swift
+++ Sources/Chart/Mark.swift
@@ -18,7 +18,7 @@ public protocol Mark {
     var fill: Color { get }
     var stroke: Color { get } // ? (https://developer.apple.com/documentation/coregraphics/cgcolor)
     var title: String { get }
-    
+
     // maybe use/infer from https://developer.apple.com/documentation/swiftui/strokestyle
     // - lineWidth: CGFloat
     // - lineCap: CGLineCap (https://developer.apple.com/documentation/coregraphics/cglinecap)
