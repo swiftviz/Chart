1309af912a8a80d7ec1a8c4ceabb597bd678160b
Joe Heck <heckj@mac.com>
2022-04-12 17:10:16 -0700

    fleshing out methods on various visual channels

--- Tests/ChartTests/LineMarkTests.swift
+++ Tests/ChartTests/LineMarkTests.swift
@@ -16,8 +16,8 @@ class LineMarkTests: XCTestCase {
 
     func testLineMarkInitializer() throws {
         let x = LineMark(data: [SampleData(xValue: 2, yValue: 3), SampleData(xValue: 3, yValue: 4)],
-                         x: QuantitativeVisualChannel(\SampleData.xValue),
-                         y: QuantitativeVisualChannel(\SampleData.yValue))
+                         x: QuantitativeVisualChannel(\.xValue),
+                         y: QuantitativeVisualChannel(\.yValue))
         XCTAssertNotNil(x)
     }
 }
