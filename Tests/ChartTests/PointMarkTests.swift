5ec9d4b8c3da348771a2bc3dbfad28a99fdf0962
Joe Heck <heckj@mac.com>
2022-05-02 13:35:53 -0700

    renaming DotMark to PointMark

--- Tests/ChartTests/PointMarkTests.swift
+++ Tests/ChartTests/PointMarkTests.swift
@@ -15,7 +15,7 @@ class DotMarkTests: XCTestCase {
     }
 
     func testDotMarkInitializer() throws {
-        let x = DotMark(data: [SampleData(xValue: 2, yValue: 3)],
+        let x = PointMark(data: [SampleData(xValue: 2, yValue: 3)],
                         x: QuantitativeVisualChannel(\.xValue),
                         y: QuantitativeVisualChannel(167))
         XCTAssertNotNil(x)
