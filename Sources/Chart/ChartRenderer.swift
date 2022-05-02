13e840b1d27d158e3084eb5b066d745eb84bf5dd
Joe Heck <heckj@mac.com>
2022-05-02 13:25:37 -0700

    fixing rendering to provide both X and Y ranges when generating marks

--- Sources/Chart/ChartRenderer.swift
+++ Sources/Chart/ChartRenderer.swift
@@ -10,28 +10,58 @@ public class ChartRenderer {
     // pre-process the collection of marks provided to determine what, if any, axis
     // and margins need to be accounted for in rendering out the view.
 
-    public func createView(_: [AnyMark]) -> some View {
+    public func createView(_ marks: [AnyMark]) -> some View {
+        
+        // init(opaque: Bool = false,
+        //      colorMode: ColorRenderingMode = .nonLinear,
+        //      rendersAsynchronously: Bool = false,
+        //      renderer: @escaping (inout GraphicsContext, CGSize) -> Void,
+        //      symbols: () -> Symbols
+        // )
         Canvas { context, size in
-            // walk the collection of marks
-            // - first determine any insets needed for axis defined within them
-            // - then iterate through the whole set, provide the range available, and get
-            //   the marks (symbolsForMark), which in turns needs to pass through or use
-            //   the range provided with the visualChannel's internal scale to get the
-            //   explicit values from the data.
+            // walk the collection of marks (`AnyMark`)
+            // - first determine any insets needed for axis defined within them (TBD)
 
-            // `symbolsForMark(rangeLower: CGFloat, rangeHigher: CGFloat) -> [MarkSymbol]`
-            // MarkSymbol is an enum with the kind of thing you're rendering and some sizes, paths
-            // either broad structs or lines, or a symbol. The symbols all return a Path
-            // shape.toPath(CGRect) -> Path
-
-            // context.stroke vs. context.fill
-            // both also take a GraphicsContext.Shading as a second parameter (rather than a Color)
-
-            context.stroke(
-                Path(ellipseIn: CGRect(origin: .zero, size: size)),
-                with: .color(.green),
-                lineWidth: 4
-            )
+            // - then calculate the marks for the provided drawing area
+            let drawArea = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+            for mark in marks {
+                // - and iterate through each of the individual symbols
+                // - render them into the canvas based on the mode of the shape
+                for marksymbol in mark.symbolsForMark(in: drawArea) {
+                    switch marksymbol {
+                    case .point(let individualPoint):
+                        let symbolRect: CGRect = CGRect(origin: CGPoint(x: individualPoint.x, y: individualPoint.y), size: individualPoint.size)
+                        switch individualPoint.shape.mode {
+                        case .stroke:
+                            context.stroke(
+                                individualPoint.shape.toPath(symbolRect),
+                                with: .color(individualPoint.shape.strokeColor),
+                                style: individualPoint.shape.style
+                            )
+                        case .fill:
+                            context.fill(
+                                individualPoint.shape.toPath(symbolRect),
+                                with: .color(individualPoint.shape.fillColor)
+                            )
+                        }
+                    case .line(let individualLine):
+                        fatalError("not yet implemented: \(individualLine)")
+                    case .rect(let individualRect):
+                        fatalError("not yet implemented: \(individualRect)")
+                    case .rule(let individualRule):
+                        fatalError("not yet implemented: \(individualRule)")
+                    case .text(let text):
+                        fatalError("not yet implemented: \(text)")
+                    case .image(let image):
+                        fatalError("not yet implemented: \(image)")
+                    }
+                }
+            }
+//            context.stroke(
+//                Path(ellipseIn: CGRect(origin: .zero, size: size)),
+//                with: .color(.green),
+//                lineWidth: 4
+//            )
         }
     }
 }
