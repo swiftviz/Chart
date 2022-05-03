0187bd366f1420efe58033493fddc51ab162ffa2
Joe Heck <heckj@mac.com>
2022-05-03 11:11:13 -0700

    switching to wrap tests in condition on macOS

--- Tests/SnapshotTests/PointChartRenderTests.swift
+++ Tests/SnapshotTests/PointChartRenderTests.swift
@@ -11,37 +11,31 @@ struct SampleData {
 private let referenceSize = CGSize(width: 300, height: 200)
 
 final class PointChartRenderTests: XCTestCase {
-    func testChartImageRendering() throws {
-        guard #available(macOS 12, *) else {
-            throw XCTSkip("Skipping test except on macOS 12+.")
+    #if os(macOS)
+        func testChartImageRendering() throws {
+            let chart = Chart {
+                PointMark(data: [SampleData(xValue: 2, yValue: 3), SampleData(xValue: 3, yValue: 5)],
+                          x: QuantitativeVisualChannel(\.xValue),
+                          y: QuantitativeVisualChannel(167))
+            }
+            assertSnapshot(
+                matching: chart.referenceFrame(),
+                as: .image(size: referenceSize)
+            )
         }
 
-        let chart = Chart {
-            PointMark(data: [SampleData(xValue: 2, yValue: 3), SampleData(xValue: 3, yValue: 5)],
-                      x: QuantitativeVisualChannel(\.xValue),
-                      y: QuantitativeVisualChannel(167))
+        func testChartImageRenderingSinglePoint() throws {
+            let chart = Chart {
+                PointMark(data: [SampleData(xValue: 2, yValue: 3)],
+                          x: QuantitativeVisualChannel(\.xValue),
+                          y: QuantitativeVisualChannel(167))
+            }
+            assertSnapshot(
+                matching: chart.referenceFrame(),
+                as: .image(size: referenceSize)
+            )
         }
-        assertSnapshot(
-            matching: chart.referenceFrame(),
-            as: .image(size: referenceSize)
-        )
-    }
-
-    func testChartImageRenderingSinglePoint() throws {
-        guard #available(macOS 12, *) else {
-            throw XCTSkip("Skipping test except on macOS 12+.")
-        }
-
-        let chart = Chart {
-            PointMark(data: [SampleData(xValue: 2, yValue: 3)],
-                      x: QuantitativeVisualChannel(\.xValue),
-                      y: QuantitativeVisualChannel(167))
-        }
-        assertSnapshot(
-            matching: chart.referenceFrame(),
-            as: .image(size: referenceSize)
-        )
-    }
+    #endif
 }
 
 private extension SwiftUI.View {
