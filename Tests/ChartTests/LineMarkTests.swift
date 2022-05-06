75d8da3c0ae291c5e18b4f042677dc4ce7dba2f3
Joe Heck <heckj@mac.com>
2022-05-06 13:25:09 -0700

    int allowable for channel
    but at the cost of exploding the type's for Marks to include every
    _possible_ channel input data type, which is going to get ugly pretty
    quickly. I suspect a better solution is to figure out how to explicitly
    _hide_ the typing of the input type so that only the data type is
    exposed.

--- Tests/ChartTests/LineMarkTests.swift
+++ Tests/ChartTests/LineMarkTests.swift
@@ -10,14 +10,37 @@ import XCTest
 
 class LineMarkTests: XCTestCase {
     struct SampleData {
+        let name: String
+        let value: Int
         let xValue: Double
         let yValue: Double
+
+        init(_ name: String, _ intValue: Int, _ x: Double, _ y: Double) {
+            self.name = name
+            value = intValue
+            xValue = x
+            yValue = y
+        }
     }
 
+    let data = [
+        SampleData("X", 3, Double.pi, 5.0),
+        SampleData("Y", 4, Double.pi, 7.0)
+    ]
+        
+
     func testLineMarkInitializer() throws {
-        let x = LineMark(data: [SampleData(xValue: 2, yValue: 3), SampleData(xValue: 3, yValue: 4)],
+        let x = LineMark(data: self.data,
                          x: QuantitativeVisualChannel(\.xValue),
                          y: QuantitativeVisualChannel(\.yValue))
         XCTAssertNotNil(x)
     }
+
+    func testDifferentLineMarkInitializer() throws {
+        let x = LineMark(data: self.data,
+                         x: QuantitativeVisualChannel(\.value),
+                         y: QuantitativeVisualChannel(\.yValue))
+        XCTAssertNotNil(x)
+    }
+
 }
