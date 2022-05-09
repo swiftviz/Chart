99b0cb16d4674c73a1334ab938643b5c9ca22063
Joe Heck <heckj@mac.com>
2022-05-09 13:56:02 -0700

    create ChartSpec and ChartBuilder
    convert [AnyMark] to ChartSpec, and migrate MarkBuilder to become
    ChartBuilder, returning a ChartSpec that can be merged one over the
    other to build up a chart declaration.

--- Sources/Chart/MarkBuilder/AnyMark.swift
+++ Sources/Chart/MarkBuilder/AnyMark.swift
@@ -10,15 +10,6 @@ import Foundation
 
 /// A type-erased Mark.
 public struct AnyMark: Mark {
-    // TODO(heckj): Might need to change how this type-erasure mechanism is working, shifting to a fully
-    // wrapped type erasure in order to allow underlying components to be manipulated in a consistent way.
-    //
-    // This closure-based type erasure hides everything except for the ability to get symbols out when
-    // its provided with a range into which they can be drawn, but there's a use case where we want to
-    // align the domains of multiple marks, with different data sets, into a single - broader and combined -
-    // domain for the representation. Currently each mark is entirely encapsulated, and there's no way after
-    // the type-erasure to "reach in" and manipulate the domains to allow them to be expanded and/or synchronized,
-    // let alone read to determine a corrected domain for each symbol.
 
     private let wrappedSymbolsForMark: (_: CGRect) -> [MarkSymbol]
 
