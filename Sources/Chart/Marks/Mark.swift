4e67bf521fd79f8949dc11568314edd9ea9e036b
Joe Heck <heckj@mac.com>
2022-05-10 12:42:59 -0700

    re-org to keep clean

---
+++ Sources/Chart/Marks/Mark.swift
@@ -0,0 +1,19 @@
+//
+//  Mark.swift
+//
+//
+//  Created by Joseph Heck on 3/25/22.
+//
+import SwiftUI
+
+/// A type that represents one or more visual symbols displays in a chart.
+///
+/// A mark describes the configuration of how data is mapped to visual properties relevant to the type of mark.
+public protocol Mark {
+    var axis: [Axis.AxisLocation: Axis] { get }
+
+    /// Creates a list of symbols to render into a rectangular drawing area that you specify.
+    /// - Parameter in: The rectangle into which to scale and draw the symbols.
+    /// - Returns: A list of symbol data structures with the information needed to draw them onto a canvas or into CoreGraphics context.
+    func symbolsForMark(in: CGRect) -> [MarkSymbol]
+}
