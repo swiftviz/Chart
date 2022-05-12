44ce3c56fc400be3dc6f0fc0a699043b00de30da
Joe Heck <heckj@mac.com>
2022-05-12 16:48:31 -0700

    working towards rendering axis information from Marks

--- Sources/Chart/Marks/AnyMark.swift
+++ Sources/Chart/Marks/AnyMark.swift
@@ -11,18 +11,20 @@ import Foundation
 /// A type-erased Mark.
 public struct AnyMark: Mark {
     public var xPropertyType: VisualPropertyType
-
     public var yPropertyType: VisualPropertyType
 
-    public var axis: [Axis.AxisLocation: Axis]
-
     private let wrappedSymbolsForMark: (_: CGRect) -> [MarkSymbol]
+    private let wrappedAxisForMark: (_: CGRect) -> [Axis.AxisLocation: Axis]
 
     public init<T: Mark>(_ specificMark: T) {
         wrappedSymbolsForMark = specificMark.symbolsForMark(in:)
-        axis = specificMark.axis
+        wrappedAxisForMark = specificMark.axisFromMark(in:)
         xPropertyType = specificMark.xPropertyType
         yPropertyType = specificMark.yPropertyType
+    }
+
+    public func axisFromMark(in rect: CGRect) -> [Axis.AxisLocation: Axis] {
+        wrappedAxisForMark(rect)
     }
 
     public func symbolsForMark(in rect: CGRect) -> [MarkSymbol] {
