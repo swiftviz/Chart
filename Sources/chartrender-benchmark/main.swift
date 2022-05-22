e6a68535ae0b2dfe5c9833659d61127ab7a19957
Joe Heck <heckj@mac.com>
2022-05-22 11:50:11 -0500

    exploring simple micro-benchmarks

--- Sources/chartrender-benchmark/main.swift
+++ Sources/chartrender-benchmark/main.swift
@@ -2,11 +2,12 @@
 //  main.swift - chartrender-benchmark
 //
 
-import SwiftUI
+import Benchmark
 import Chart
+import SwiftUI
 
 // swift build --target chartrender-benchmark -c release
-//
+// .build/release/chartrender-benchmark --time-unit ms --quiet
 //
 let referenceSize = CGSize(width: 300, height: 200)
 
@@ -28,12 +29,35 @@ let data: [SampleData] = [
     SampleData(name: "D", value: 7),
 ]
 
+benchmark("create chart") {
+//    for _ in 1...100 {
+    let chart = Chart {
+        BarMark(data: data,
+                value: QuantitativeVisualChannel(\.value),
+                category: BandVisualChannel(\.name))
+    }
+//    }
+}
+
+benchmark("snapshot chart") {
+    let chart = Chart {
+        BarMark(data: data,
+                value: QuantitativeVisualChannel(\.value),
+                category: BandVisualChannel(\.name))
+    }
+//    for _ in 1...100 {
+    let image = chart.snapshot()
+//    }
+}
+
+Benchmark.main()
+
 extension View {
     func snapshot() -> NSImage? {
         let controller = NSHostingController(rootView: self)
         let targetSize = controller.view.intrinsicContentSize
         let contentRect = NSRect(origin: .zero, size: targetSize)
-        
+
         let window = NSWindow(
             contentRect: contentRect,
             styleMask: [.borderless],
@@ -41,26 +65,14 @@ extension View {
             defer: false
         )
         window.contentView = controller.view
-        
+
         guard
             let bitmapRep = controller.view.bitmapImageRepForCachingDisplay(in: contentRect)
         else { return nil }
-        
+
         controller.view.cacheDisplay(in: contentRect, to: bitmapRep)
         let image = NSImage(size: bitmapRep.size)
         image.addRepresentation(bitmapRep)
         return image
     }
 }
-
-func testImageRendering() {
-    let chart = Chart {
-        BarMark(data: data,
-                value: QuantitativeVisualChannel(\.value),
-                category: BandVisualChannel(\.name))
-    }
-    let image = chart.snapshot()
-    print(String(describing: image))
-}
-
-testImageRendering()
