3ce6a97fe2e764875cde8d19554a2cb5c1fffac6
Joe Heck <heckj@mac.com>
2022-05-22 07:45:17 -0500

    benchmark code stub

---
+++ Sources/chartrender-benchmark/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift - chartrender-benchmark
+//  
+
+import Foundation
+import Chart
+import SnapshotTesting
+//import CollectionsBenchmark
+//import Kalimba
+//
+//// Create a new benchmark instance.
+//var benchmark = Benchmark(title: "Kalimba")
+//
+//// Define a very simple benchmark called `kalimbaOrdered`.
+//benchmark.addSimple(
+//  title: "kalimbaOrdered",
+//  input: [Int].self
+//) { input in
+//  blackHole(input.kalimbaOrdered())
+//}
+//
+//// Execute the benchmark tool with the above definitions.
+//benchmark.main()
