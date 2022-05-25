6fceca93530c2abbefdaad4b105d413302d771f7
Joe Heck <heckj@mac.com>
2022-05-25 10:46:08 -0700

    moving tick calculations into the axis struct itself

--- Sources/Chart/Marks/AnyMark.swift
+++ Sources/Chart/Marks/AnyMark.swift
@@ -13,19 +13,13 @@ public struct AnyMark: Mark {
     public var _xAxis: Axis?
 
     private let wrappedSymbolsForMark: (_: CGRect) -> [Sigil]
-    private let wrappedAxisForMark: (_: CGRect) -> [Axis]
 
     public init<T: Mark>(_ specificMark: T) {
         wrappedSymbolsForMark = specificMark.symbolsForMark(in:)
         xPropertyScale = specificMark.xPropertyScale
         yPropertyScale = specificMark.yPropertyScale
-        wrappedAxisForMark = specificMark.axisForMark(in:)
         _xAxis = specificMark._xAxis
         _yAxis = specificMark._yAxis
-    }
-
-    public func axisForMark(in rect: CGRect) -> [Axis] {
-        wrappedAxisForMark(rect)
     }
 
     public func symbolsForMark(in rect: CGRect) -> [Sigil] {
