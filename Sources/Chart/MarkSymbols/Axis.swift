9ee230536f9bdd93fb4ad947c5c19cae6db96da5
Joe Heck <heckj@mac.com>
2022-05-26 15:50:34 -0700

    commenting out un-needed code

--- Sources/Chart/MarkSymbols/Axis.swift
+++ Sources/Chart/MarkSymbols/Axis.swift
@@ -139,26 +139,10 @@ public struct Axis {
         ticks = []
     }
 
-    func addingTicks(_ ticks: [Tick<CGFloat>]) -> Self {
-        var copy = self
-        copy.ticks = ticks
-        return copy
-    }
-
-//    /// Returns a copy of the axis with ticks and locations calculated for the rectangular area you provide.
-//    /// - Parameter rect: The rectangle into which to scale and draw axis.
-//    ///
-//    /// The default implementation for a ``Chart/Axis`` starts with no ticks available until you calculate
-//    /// their positions by providing a region into which the axis will be drawn using this method.
-//    func withTicksIn(_ rect: CGRect) -> Self {
-//        switch axisLocation {
-//        case .top, .bottom:
-//            let ticks = scale.tickValuesFromScale(lower: rect.origin.x, higher: rect.origin.x + rect.width, values: requestedTickValues)
-//            return addingTicks(ticks)
-//        case .leading, .trailing:
-//            let ticks = scale.tickValuesFromScale(reversed: true, lower: rect.origin.y, higher: rect.origin.y + rect.height, values: requestedTickValues)
-//            return addingTicks(ticks)
-//        }
+//    func addingTicks(_ ticks: [Tick<CGFloat>]) -> Self {
+//        var copy = self
+//        copy.ticks = ticks
+//        return copy
 //    }
 
     /// Resolves the ticks with labels and positions for the rectangular area that you provide.
