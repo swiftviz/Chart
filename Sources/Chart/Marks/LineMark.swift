c8d46a4a729b7dae7730b99dd3d9584fed9328ec
Joe Heck <heckj@mac.com>
2022-05-31 14:25:54 -0700

    line marks using reversed Y directly

--- Sources/Chart/Marks/LineMark.swift
+++ Sources/Chart/Marks/LineMark.swift
@@ -41,24 +41,24 @@ public struct LineMark<DataSource>: Mark, MarkAxis {
     public func symbolsForMark(in rect: CGRect) -> [Sigil] {
         // - apply the range onto the various VisualChannel scales, or pass it along when creating
         //   the symbols with final values. (from VisualChannel.provideScaledValue()
-        let xScale = x.range(rangeLower: 0, rangeHigher: rect.size.width)
-        let yScale = y.range(rangeLower: 0, rangeHigher: rect.size.height)
+        let xScale = x.range(rangeLower: rect.origin.x,
+                             rangeHigher: rect.origin.x + rect.size.width)
+        let yScale = y.range(reversed: true,
+                             rangeLower: rect.origin.y,
+                             rangeHigher: rect.origin.y + rect.size.height)
         var symbols: [Sigil] = []
-//        print("Creating symbols within rect: \(rect)")
-//        print("X scale: \(xScale)")
-//        print("Y scale: \(yScale)")
+
         var previousData: DataSource?
         for pointData in data {
             if let xValue = xScale.scaledValue(data: pointData),
                let yValue = yScale.scaledValue(data: pointData)
             {
                 let newPoint = IndividualPoint(
-                    x: rect.origin.x + xValue,
-                    y: rect.height + rect.origin.y - yValue,
+                    x: xValue,
+                    y: yValue,
                     shape: PlotShape(Circle()), size: 5
                 )
                 symbols.append(.point(newPoint))
-//                print(" .. \(newPoint)")
                 // There won't be a line for the first point, but all following points
                 // include a line from the current point back to the previous point.
                 if let previousData = previousData,
@@ -66,14 +66,13 @@ public struct LineMark<DataSource>: Mark, MarkAxis {
                    let y2Value = yScale.scaledValue(data: previousData)
                 {
                     let lineBack = IndividualLine(
-                        x1: rect.origin.x + xValue,
-                        y1: rect.height + rect.origin.y - yValue,
-                        x2: rect.origin.x + x2Value,
-                        y2: rect.height + rect.origin.y - y2Value,
+                        x1: xValue,
+                        y1: yValue,
+                        x2: x2Value,
+                        y2: y2Value,
                         size: 1
                     )
                     symbols.append(.line(lineBack))
-//                    print(" .. \(lineBack)")
                 }
             }
             previousData = pointData
