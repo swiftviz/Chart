0c82ec9e09861841015dda39e09983b2854734d5
Joe Heck <heckj@mac.com>
2022-06-01 17:47:54 -0700

    exploring with interactive views

--- Sources/ExampleChartViews/SwiftUIView.swift
+++ Sources/ExampleChartViews/SwiftUIView.swift
@@ -5,11 +5,18 @@
 //  Created by Joseph Heck on 5/31/22.
 //
 
+import Chart
+// conceptually, I'm hoping to create views that loosely match the kind of interactive exploration you can find with Observable's Plot at
+// https://observablehq.com/@observablehq/plot-cheatsheets
 import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text("hi")
+        Chart(_options: [.all]) {
+            PointMark(data: [1, 2, 3, 10, 11], x: QuantitativeVisualChannel(13), y: QuantitativeVisualChannel(\.self))
+                .xAxis(showTickLabels: false, label: "fredd", labelOffset: 10)
+                .yAxis()
+        }.frame(width: 400, height: 200, alignment: .center)
     }
 }
 
